13b8d9007fd4f33ecf0ef4c7d214ca87089876f1
PascalLP <pascallabellep_2000@outlook.com>
2024-05-16 15:30:41 -0400

    Added terrain

---
+++ Assets/Code/Scripts/StateMachines/PlayerMachines/PlayerBaseState.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerBaseState : State
+{
+    protected readonly PlayerStateMachine stateMachine;
+
+    protected PlayerBaseState(PlayerStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    protected void CalculateMoveDirection()
+    {
+        Vector3 cameraForward = new(stateMachine.MainCamera.forward.x, 0, stateMachine.MainCamera.forward.z);
+        Vector3 cameraRight = new(stateMachine.MainCamera.right.x, 0, stateMachine.MainCamera.right.z);
+
+        Vector3 moveDirection = cameraForward.normalized * stateMachine.InputReader.MoveComposite.y + cameraRight.normalized * stateMachine.InputReader.MoveComposite.x;
+
+        stateMachine.Velocity.x = stateMachine.Speed * moveDirection.x * stateMachine.MovementSpeed;
+        stateMachine.Velocity.z = stateMachine.Speed * moveDirection.z * stateMachine.MovementSpeed;
+    }
+
+    protected void FaceMoveDirection()
+    {
+        Vector3 faceDirection = new(stateMachine.Velocity.x, 0f, stateMachine.Velocity.z);
+
+        if (faceDirection == Vector3.zero)
+            return;
+
+        stateMachine.transform.rotation = Quaternion.Slerp(stateMachine.transform.rotation, Quaternion.LookRotation(faceDirection), stateMachine.LookRotationDampFactor * Time.deltaTime);
+    }
+
+    protected void ApplyGravity()
+    {
+        if (stateMachine.Velocity.y > Physics.gravity.y)
+        {
+            stateMachine.Velocity.y += Physics.gravity.y * Time.deltaTime;
+        }
+    }
+
+    protected void Move()
+    {
+        stateMachine.Controller.Move(stateMachine.Velocity * Time.deltaTime);
+    }
+
+    // Added these implementations to avoid error messages v
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Tick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Code/Scripts/StateMachines/StateMachine.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    private State currentState;
+
+    public void SwitchState(State state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState.Enter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState?.Tick();
+    }
+}
